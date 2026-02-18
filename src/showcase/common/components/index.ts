14dd40188debc1c443ac7ff09b5ca68d50bfa973
BinaryJack <pianatadeo@hotmail.com>
2026-02-18 15:55:55 +0100

    feat: Add Modal component and integrate with PortalSlots in ModalPortalDemo

--- src/showcase/common/components/index.ts
+++ src/showcase/common/components/index.ts
@@ -8,5 +8,6 @@ export { ConsoleLogViewer } from './console-log-viewer.psr';
 export type { LogEntry } from './console-log-viewer.psr';
 export { DemoButton } from './demo-button.psr';
 export { DemoSection } from './demo-section.psr';
+export { Modal } from './modal.psr';
 export { PageHeader } from './page-header.psr';
 export { StatusIndicator } from './status-indicator.psr';
